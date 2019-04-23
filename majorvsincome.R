914df87967c93b3223f831f9f99429cc5f752055
himank <himank369123@gmail.com>
2019-04-23 22:53:19 +0530

    first commit

---
+++ majorvsincome.R
@@ -0,0 +1,20 @@
+devtools::install_github("jhudsl/collegeIncome")
+library(collegeIncome)
+data(college)
+devtools::install_github("jhudsl/matahari")
+library(matahari)
+dance_start(value = FALSE, contents = FALSE)
+str(college)
+fit<-lm(median~major_category,data=college)
+hist(college$median)
+any(is.na(college$major_category))
+factor(college$major_category)
+df<-aggregate(college$median~college$major_category,FUN = mean)
+names(df)<-c("major","medsal")
+par(mfrow=c(1,1))
+barplot(df$medsal)
+summary(fit)$coef
+summary(fit)$coef[,4]<0.05
+
+#the p values suggest that the medians are not significantly different.
+dance_save("C:/Users/himank/Documents/college_major_analysis.rds")
